bac51d3f7d769fc7fd7fb7b00d50a1829e05141c
ahmedshakshak <ahmedshakshak.fcis@gmail.com>
2019-05-13 22:30:09 +0200

    Update Project.cs

--- TaskManager/TaskManager/Project.cs
+++ TaskManager/TaskManager/Project.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace TaskManager
 {
     public class Project
     {
+        static public List<Project> projects = new List<Project>();
         public int id;
         public string name, description;
         public List<Employee> emploees;
@@ -15,8 +16,25 @@ namespace TaskManager
         {
              emploees=new List<Employee>();
         }
+
+        static public void readProjects()
+        {
+            FileStream fileStream = new FileStream("Projects.xml", FileMode.OpenOrCreate);
+            XmlSerializer xmlSerializer = new XmlSerializer(projects.GetType());
+
+            if (fileStream.Length != 0)
+                projects = (List<Project>)xmlSerializer.Deserialize(fileStream);
+
+            fileStream.Close();
+        }
+
+        static public void writeProjects()
+        {
+            FileStream fileStream = new FileStream("Projects.xml", FileMode.OpenOrCreate);
+            XmlSerializer xmlSerializer = new XmlSerializer(projects.GetType());
+            xmlSerializer.Serialize(fileStream, projects);
+            fileStream.Close();
+        }
     }
 }
 ////projecs 
-////employess
-////tasks
